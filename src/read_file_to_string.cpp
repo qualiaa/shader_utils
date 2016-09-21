aa8e5e9a6a5b7b458b98381d40510c531ee8edd0
Jamie Bayne <jamiebayne@hotmail.com>
2016-09-21 19:30:49 +0100

    Throw exception on shader file not found

--- src/read_file_to_string.cpp
+++ src/read_file_to_string.cpp
@@ -1,11 +1,16 @@
 #include "read_file_to_string.hpp"
 #include <sstream>
+#include <stdexcept>
 #include <fstream>
 
 std::string read_file_to_string(std::string const& file_path)
 {
-    // XXX: Point of failure - no file
     std::ifstream current_file{file_path};
+    if (not current_file) {
+        std::stringstream errstream;
+        errstream << "Could not open file '" << file_path << '\'';
+        throw std::runtime_error(errstream.str());
+    }
     std::stringstream sstream;
     sstream << current_file.rdbuf();
     return sstream.str();
